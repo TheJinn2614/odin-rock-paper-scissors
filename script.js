5cfb930d584cc3977f48e9e2dd7e633abfba551a
TheJinn2614 <ivanstoev2614@gmail.com>
2022-03-21 00:01:31 +0000

    Finished computer and user pick functions

---
+++ script.js
@@ -0,0 +1,16 @@
+let computerPlay = () => {
+    const words = ["Rock", "Paper", "Scissors"];
+    let num = Math.floor(Math.random() * 3);
+    let computerResult = words[num];
+    // console.log(computerResult);
+}
+// computerPlay();
+
+let userPlay = () => {
+    const words = ["Rock", "Paper", "Scissors"];
+    let num = Math.floor(Math.random() * 3);
+    let userResult = words[num];
+    // console.log(userResult)
+}
+// userPlay();
+
