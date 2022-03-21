ebe7ec16be7969ea8424785f8f5545f41280b388
TheJinn2614 <ivanstoev2614@gmail.com>
2022-03-21 00:39:56 +0000

    Finished playRound func

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ let userPlay = () => {
 }
 // userPlay();
 
+let playRound = (userSelection, computerSelection) => {
+
+    // dont forget to come back and .toLowerCase
+
+    // This could also be done with switch but I prefer if statements
+    if (userSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock"
+    } else if (userSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper"
+    } else if (userSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats Scissors"
+    } else if (userSelection === "rock" && computerSelection === "rock") {
+        return "It's a tie! Rock and Rock"
+    } else if (userSelection === "paper" && computerSelection === "paper") {
+        return "It's a tie! Paper and Paper"
+    } else if (userSelection === "scissors" && computerSelection === "scissors") {
+        return "It's a tie! Scissors and Scissors"
+    }
+}
+
+playRound(userPlay, computerPlay);
